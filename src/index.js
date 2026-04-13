f5d8142e5edd57f78e8d9ecc2f937b7e2a6aa4e5
Ian <98838549+i1986o@users.noreply.github.com>
2026-04-12 23:27:46 -0400

    feat: enable Spicerack source in aggregator

--- src/index.js
+++ src/index.js
@@ -3,11 +3,12 @@
  */
 import { config } from "../config.js";
 import { fetchWizardsEvents } from "./sources/wizards-locator.js";
+import { fetchSpicerackEvents } from "./sources/spicerack.js";
 import { generateIcal } from "./output/ical.js";
 import { dedupe, sortByDate, logSummary } from "./utils.js";
 
 async function main() {
-  console.log("🃏 MTG Calendar Aggregator");
+  console.log("\ud83c\udccf MTG Calendar Aggregator");
   console.log(`   Location: ${config.location.city}, ${config.location.state} (${config.location.zip})`);
   console.log(`   Radius: ${config.searchRadiusMiles} miles`);
   console.log(`   Days ahead: ${config.daysAhead}`);
@@ -15,13 +16,22 @@ async function main() {
     console.log(`   Format filter: ${config.formatFilter.join(", ")}`);
   }
   console.log();
+
   const allEvents = [];
+
   if (config.sources.wizardsLocator) {
-    const wotcEvents = await fetchWizardsEvents();
-    allEvents.push(...wotcEvents);
+    const events = await fetchWizardsEvents();
+    allEvents.push(...events);
   }
+
+  if (config.sources.spicerack) {
+    const events = await fetchSpicerackEvents();
+    allEvents.push(...events);
+  }
+
   const deduped = dedupe(allEvents);
   const sorted = sortByDate(deduped);
+
   logSummary(sorted);
   generateIcal(sorted);
 }
